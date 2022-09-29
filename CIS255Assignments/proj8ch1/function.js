f62595df82bcd749b8ebf6124c14a81a96b5ea69
sarah <smmentel@svsu.edu>
2022-09-29 09:52:21 -0400

    proj 1 ch 8

--- CIS255Assignments/proj8ch1/function.js
+++ CIS255Assignments/proj8ch1/function.js
@@ -1,1 +1,21 @@
 /* define your functions here */
+
+/* Function to calculate the product total */
+function calculateTotal(quantity, price){
+    return  quantity * price;
+}
+
+/*Function to return Cart Row */
+function outputCartRow(item, total){
+    var table = document.getElementById("table-fill");
+  var row = table.insertRow(0);
+
+  for (let i = 0; i < 9; i++) {
+    var i = table.insertRow(i);
+  }
+
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
