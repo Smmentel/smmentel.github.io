0d3d539768de5b59524a4b3e6a1d98776c681983
Smmentel <108294106+Smmentel@users.noreply.github.com>
2022-10-13 16:13:49 -0400

    Update function.js

--- CIS255Assignments/proj8ch1/function.js
+++ CIS255Assignments/proj8ch1/function.js
@@ -7,16 +7,56 @@ function calculateTotal(quantity, price){
 
 /*Function to return Cart Row */
 function outputCartRow(item, total){
-    var table = document.getElementById("table-fill")[0];
+  
+  //prompt for tax and shipMinimum
+ let tax = parseFloat(prompt("What is the tax?", "0.10"));
+ let shipMinimum = parseFloat(prompt("What is the shipping threshold?", "300"))
+    
+ //create table
+  var table = document.getElementById("table-fill")[0];
   var row = table.insertRow(0);
-
-  for (let i = 0; i < 9; i++) {
-    var i = table.insertRow(i);
+//fill with rows
+  for (let i = 1; i < cart.length; i++) {
+    var x = table.insertRow(i);
   }
-  for (let i = 0; i < 5; i++) {
-    var i = row.insertCell(i);
+ //fill rows with cells
+  for (let j = 0; j < 5; j++) {
+    var y = row.insertCell(j);
   }
  
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
+  //
+  for(let n = 0; n < cart.length; n++){
+   
+  let quantity = cart[i].quantity;
+  let price = cart[i]
+  let curTotal = calculateTotal(quantity, price).toFixed(2);
+    subtotal += parseFloat(curTotal);
+  
+  cell0.innerHTML = "<img src = images/" + cart[i].product.filename + ">"; // image
+    cell1.innerHTML = cart[i].product.title; // product
+    cell2.innerHTML = quantity;              // quantity
+    cell3.innerHTML = "$" + price;                 // price
+    cell4.innerHTML = "$" + curTotal;    // total
+    
+    //Align Text and adjust image table cell
+    cell0.style.width = "140px";
+    cell2.style.textAlign = "center";
+    cell3.style.textAlign = "center";
+    cell4.style.textAlign = "right";
+    }
+    
+    let shipping = (subtotal < shipMinimum) ? 40 : 0
+
+    //After page load, update bottom of table with correct total amounts
+    let totals = document.getElementsByClassName('totals');
+    totals[0].getElementsByTagName("td")[1].innerHTML = "$" + subtotal.toFixed(2);
+    totals[1].getElementsByTagName("td")[1].innerHTML = "$" + (subtotal * tax).toFixed(2);
+    totals[2].getElementsByTagName("td")[1].innerHTML = "$" + shipping.toFixed(2);
+    totals[3].getElementsByTagName("td")[1].innerHTML = "$" + parseFloat(subtotal + (subtotal * tax) + shipping).toFixed(2);
+    
+    
+
+
+outputCartRow();
+  
 }
